30c2984013fd6cf8d889e44432d562150e05dccf
emerald eto <officialemeraldeto@gmail.com>
2023-07-18 09:43:59 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+function changeBackground(x){
+   console.log(x.value);
+   var h1 = document.getElementById('h1');
+   heading.style.backgroundColor = x.value;
+}
+
+function changeBackground(x){
+   console.log(x.value);
+   var body = document.getElementById('body');
+   body.style.backgroundColor = x.value;
+   
+}
+
+
